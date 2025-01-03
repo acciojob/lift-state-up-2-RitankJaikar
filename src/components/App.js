c4c9b0003d1d1de53393e0d4c1f04be35488445e
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-03 22:11:28 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from 'react';
+import Child from './Child';
 
 const App = () => {
+  const [inputValue, setInputValue] = useState(''); // State to hold the value of the input
+
+  // Method to handle the change of input
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Lift State Up Example</h1>
+      {/* Passing inputValue and handleInputChange as props to Child */}
+      <Child 
+        inputValue={inputValue} 
+        onInputChange={handleInputChange} 
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
